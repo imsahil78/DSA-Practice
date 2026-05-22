ea3711d82be2f45034ee0748f5a7c1839e87496a
Sahil Vanjare <sahilvanjare78@gmail.com>
2026-05-22 17:41:09 +0530

    Add Sort Colors solution

---
+++ Sorting/sort_colors.cpp
@@ -0,0 +1,47 @@
+
+### Approach
+
+Count the number of 0s, 1s, and 2s, then place them back into the array in sorted order.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] == 0) count0++ ;
+            if(nums[i] == 1) count1++ ;
+            if(nums[i] == 2) count2++ ;
+        }
+
+        int index = 0;
+
+        while(count0--)
+        {
+            nums[index] = 0 ;
+            index ++ ;
+        }
+
+           while(count1--)
+        {
+            nums[index] = 1 ;
+            index ++ ;
+        }
+
+           while(count2--)
+        {
+            nums[index] = 2;
+            index ++ ;
+        }
+
+
+    }
+};
